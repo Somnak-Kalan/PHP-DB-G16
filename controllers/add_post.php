19361b0c9209e14484774927cc1c8db75cc723b4
sophalS <sophal.sararn@student.passerellesnumeriques.org>
2022-03-19 18:41:36 +0700

    #2 add foreign key to post table

--- controllers/add_post.php
+++ controllers/add_post.php
@@ -6,10 +6,11 @@ if($_SERVER['REQUEST_METHOD']=='POST')
 {
     $content=$_POST['content'];
     $img=$_FILES['img']['name'];
+    $user_id=$_GET['user_id'];
     $target='../images/'.$img;
     move_uploaded_file($_FILES['img']['tmp_name'],$target);
-    if(!empty($content) or $img) {
-        add_post($content,$img);
+    if(!empty($content) or $img or $user_id) {
+        add_post($content,$img,$user_id);
         
     };
     header('Location:../index.php') ; 
