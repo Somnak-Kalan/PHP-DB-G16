3140c055a336fc6533b4678f4242bd70ced6367b
Somnak-Kalan <kalansomnak12@gmail.com>
2022-03-28 13:48:10 +0700

    like

--- controllers/add_like.php
+++ controllers/add_like.php
@@ -1,7 +1,17 @@
 <?php require_once('../models/like.php');
-$id =$_GET['id'];
+$post_id =$_GET['id'];
 $user_id=$_GET['user_id'];
-if(($id  and $user_id )!==null){
-    insert_like($id,$user_id);
+$get_list_like = stop_like($_GET['id']);
+$is_the_same_user = true;
+foreach($get_list_like as $like){
+    if($user_id==$_GET['user_id']){
+        $is_the_same_user=false;
+    };
 }
-header('location:../home_view.php');
+if($is_the_same_user){
+    insert_like($post_id,$user_id);
+    header('location:../home_view.php');
+}else{
+    remove_like($post_id,$user_id);
+    header('location:../home_view.php');
+}
